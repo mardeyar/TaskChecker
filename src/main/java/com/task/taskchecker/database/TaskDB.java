4a1e0313195695b73a46bae961ad3aba5c866917
Mark Deyarmond <deyarmondmark@gmail.com>
2024-02-22 13:43:42 -0400

    Removed taskInfo

--- src/main/java/com/task/taskchecker/database/TaskDB.java
+++ src/main/java/com/task/taskchecker/database/TaskDB.java
@@ -11,8 +11,8 @@ import java.util.List;
 public class TaskDB implements TaskDAO {
     private static final String SQL_SELECT = "SELECT * FROM task_list";
     private static final String SQL_SELECT_ONE = "SELECT * FROM task_list WHERE task_id = ?";
-    //private static final String SQL_INSERT = "INSERT INTO task_list(task_name, task_info, task_status, due_date) VALUES (?,?,?,?)";
-    private static final String SQL_UPDATE = "UPDATE task_list SET task_name=?, task_info=?, task_status=?, due_date=? WHERE task_id = ?";
+    private static final String SQL_INSERT = "INSERT INTO task_list(task_name, task_status, due_date) VALUES (?,?,?)";
+    private static final String SQL_UPDATE = "UPDATE task_list SET task_name=?, task_status=?, due_date=? WHERE task_id = ?";
     private static final String SQL_DELETE = "DELETE FROM task_list WHERE task_id = ?";
 
     // Insert the methods from the TaskDAO here
@@ -27,13 +27,12 @@ public class TaskDB implements TaskDAO {
         // Attempt some data insertion
         try {
             connection = getConnection();
-            preparedStatement = connection.prepareStatement("INSERT INTO task_list(task_name, task_info, task_status, due_date) VALUES (?,?,?,?)");
+            preparedStatement = connection.prepareStatement(SQL_INSERT);
 
             // This block runs a prepared statement to insert the data
             preparedStatement.setString(1, task.getTaskName());
-            preparedStatement.setString(2, task.getTaskInfo());
-            preparedStatement.setString(3, task.getTaskStatus());
-            preparedStatement.setDate(4, (Date) task.getDueDate());
+            preparedStatement.setString(2, task.getTaskStatus());
+            preparedStatement.setDate(3, (Date) task.getDueDate());
 
             preparedStatement.executeUpdate();
         } catch (SQLSyntaxErrorException e) {
@@ -59,10 +58,9 @@ public class TaskDB implements TaskDAO {
             preparedStatement = connection.prepareStatement(SQL_UPDATE);
 
             preparedStatement.setString(1, task.getTaskName());
-            preparedStatement.setString(2, task.getTaskInfo());
-            preparedStatement.setString(3, task.getTaskStatus());
-            preparedStatement.setDate(4, (Date) task.getDueDate());
-            preparedStatement.setInt(5, task.getTaskId());
+            preparedStatement.setString(2, task.getTaskStatus());
+            preparedStatement.setDate(3, (Date) task.getDueDate());
+            preparedStatement.setInt(4, task.getTaskId());
 
             resultSet = preparedStatement.executeUpdate();
         } catch (SQLSyntaxErrorException e) {
@@ -87,7 +85,7 @@ public class TaskDB implements TaskDAO {
             connection = getConnection();
             preparedStatement = connection.prepareStatement(SQL_DELETE);
             preparedStatement.setInt(1, taskId);
-            resultSet = preparedStatement.executeUpdate();
+            preparedStatement.executeUpdate();
         } catch (SQLSyntaxErrorException e) {
             System.err.println("SQL Syntax error: " + e.getMessage());
         } catch (ClassNotFoundException e) {
@@ -126,7 +124,6 @@ public class TaskDB implements TaskDAO {
                 tasks.add(new Task(
                         resultSet.getInt("task_id"),
                         resultSet.getString("task_name"),
-                        resultSet.getString("task_info"),
                         resultSet.getString("task_status"),
                         resultSet.getDate("due_date")
                 ));
