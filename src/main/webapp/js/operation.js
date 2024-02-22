85086c2bbbb83266de360ab39532988d9a8b15f6
Mark Deyarmond <deyarmondmark@gmail.com>
2024-02-22 13:42:53 -0400

    Created JS file to allow inline task edits

---
+++ src/main/webapp/js/operation.js
@@ -0,0 +1,9 @@
+function edit(taskId) {
+    const taskName = document.getElementById("taskName_" + taskId);
+    const taskStatus = document.getElementById("taskStatus_" + taskId);
+    const dueDate = document.getElementById("dueDate_" + taskId);
+}
+
+function save(taskId) {
+
+}
