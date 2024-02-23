bec652ed45931638335f8e575f48e3550715cc5c
Mark Deyarmond <deyarmondmark@gmail.com>
2024-02-23 13:15:24 -0400

    Removed js file

--- src/main/webapp/js/operation.js
+++
@@ -1,9 +0,0 @@
-function edit(taskId) {
-    const taskName = document.getElementById("taskName_" + taskId);
-    const taskStatus = document.getElementById("taskStatus_" + taskId);
-    const dueDate = document.getElementById("dueDate_" + taskId);
-}
-
-function save(taskId) {
-
-}
