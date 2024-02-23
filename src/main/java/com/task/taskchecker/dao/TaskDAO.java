4a32a0d0b0d8131dac8c0649d5dafda6d81d477a
Mark Deyarmond <deyarmondmark@gmail.com>
2024-02-23 13:18:45 -0400

    Removed unnecessary select method

--- src/main/java/com/task/taskchecker/dao/TaskDAO.java
+++ src/main/java/com/task/taskchecker/dao/TaskDAO.java
@@ -8,8 +8,7 @@ import java.util.List;
 public interface TaskDAO {
 
     void insert(Task task) throws SQLException;
-    void update(Task task) throws SQLException;
+    void update(int taskId) throws SQLException;
     void delete(int taskId) throws SQLException;
-    //public Task select(int taskId) throws SQLException;
     List<Task> select() throws SQLException;
 }
